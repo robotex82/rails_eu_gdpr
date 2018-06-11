466264266ea150ad508872b12e53d66b3b3b1d76
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-06-11 14:26:42 +0200

    Fix view path resolving for ecm_cms.

--- app/resolvers/eu_gdpr/privacy_policy_resolver.rb
+++ app/resolvers/eu_gdpr/privacy_policy_resolver.rb
@@ -1,4 +1,4 @@
 module EuGdpr
   class PrivacyPolicyResolver < Ecm::Cms::PageResolver
   end
-end if Gem.loaded_specs["ecm_cms2"].present?
+end if Gem.loaded_specs["ecm_cms"].present? || Gem.loaded_specs["ecm_cms2"].present?
