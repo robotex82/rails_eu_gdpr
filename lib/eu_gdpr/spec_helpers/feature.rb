ec37e3a16c5cb943a4b99f54d80053fec202ce69
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-10-09 13:08:12 +0200

    Fix spec helper.

--- lib/eu_gdpr/spec_helpers/feature.rb
+++ lib/eu_gdpr/spec_helpers/feature.rb
@@ -2,7 +2,7 @@ module EuGdpr
   module SpecHelpers
     module Feature
       def accept_eu_gdrp_cookies
-        find('#eu-gdpr-cookie-consent-accept').click
+        find('#eu-gdpr-cookies-accept').click
       end
     end
   end
