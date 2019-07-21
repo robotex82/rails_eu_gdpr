6cd1897481a8b15952818c95f6393684007d7e9a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-07-21 22:25:37 +0200

    Fix support cor cmor.

--- app/resolvers/eu_gdpr/privacy_policy_resolver.rb
+++ app/resolvers/eu_gdpr/privacy_policy_resolver.rb
@@ -2,3 +2,8 @@ module EuGdpr
   class PrivacyPolicyResolver < Ecm::Cms::PageResolver
   end
 end if Gem.loaded_specs["ecm_cms"].present? || Gem.loaded_specs["ecm_cms2"].present?
+
+module EuGdpr
+  class PrivacyPolicyResolver < Cmor::Cms::PageResolver
+  end
+end if Gem.loaded_specs["cmor_cms"].present?
