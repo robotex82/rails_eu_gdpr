b9a4e6e150f3f3dbb669037b66ff4d6245cfe145
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-10-22 22:30:39 +0200

    Fix privacy policy not being found when using cmor_cms.

--- app/controllers/eu_gdpr/privacy_policies_controller.rb
+++ app/controllers/eu_gdpr/privacy_policies_controller.rb
@@ -1,6 +1,6 @@
 module EuGdpr
   class PrivacyPoliciesController < EuGdpr::Configuration.base_controller.constantize
-    if Gem.loaded_specs["ecm_cms"].present? || Gem.loaded_specs["ecm_cms2"].present?
+    if Gem.loaded_specs["ecm_cms"].present? || Gem.loaded_specs["ecm_cms2"].present? || Gem.loaded_specs["cmor_cms"].present?
       prepend_view_path ::EuGdpr::PrivacyPolicyResolver.instance unless view_paths.include?(::EuGdpr::PrivacyPolicyResolver.instance)
     end
 
